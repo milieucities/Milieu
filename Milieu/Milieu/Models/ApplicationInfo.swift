8b67581329b7607692e65b1199827463e2841ad1
Xiaoxi Pang <xiaoxi.pang@inbaytech.com>
2016-01-24 03:20:16 -0500

    #47 Improvements: 	- Preload some vacants sites

--- Milieu/Milieu/Models/ApplicationInfo.swift
+++ Milieu/Milieu/Models/ApplicationInfo.swift
@@ -21,6 +21,11 @@ class ApplicationInfo : MilieuAnnotation{
     let devId: String?
     let devSiteUid: Int?
     let type: String?
+    let preDefinedVacants = ["D07-12-14-0197","D01-01-13-0003","D01-01-14-0021","D07-12-13-0119",
+        "D01-01-14-0020","D07-05-15-0005","D07-12-14-0194","D02-02-13-0042","D07-12-14-0169",
+        "D07-12-15-0050","D01-01-14-0026","D02-02-15-0019","D07-05-15-0006","D07-05-15-0001",
+        "D02-02-14-0143","D07-12-15-0119","D02-02-15-0026","D02-02-15-0044","D07-12-15-0081",
+        "D02-02-15-0019","D07-12-15-0086","D07-16-15-0003"]
     
     init(devApp: DevApp) {
         let devAppAddress = devApp.addresses?.allObjects.first as? Address
@@ -36,6 +41,16 @@ class ApplicationInfo : MilieuAnnotation{
         type = devApp.applicationType
         
         var appCategory: AnnotationCategory = AnnotationCategory.General
+        
+        for vacant in preDefinedVacants{
+            if devId == vacant{
+                appCategory = AnnotationCategory.Vacant
+            }
+        }
+        
+        if type == "Demolition Control"{
+            appCategory = AnnotationCategory.Vacant
+        }
         
         // TODO: Change the status to a NSOrderedSet!
         if let statuses: NSSet = devApp.statuses{
