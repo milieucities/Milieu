d7a61624680928a82375c7ff99dc353e30b3d65e
Xiaoxi Pang <pangxiaoxi@gmail.com>
2016-09-14 20:54:52 -0400

    MMA-3 Intermediate Change: 	- Fix the Alamofire

---
+++ Milieu/Pods/Target Support Files/STPopup/STPopup-umbrella.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+
+#import "STPopup.h"
+#import "STPopupController.h"
+#import "STPopupControllerTransitioningFade.h"
+#import "STPopupControllerTransitioningSlideVertical.h"
+#import "STPopupLeftBarItem.h"
+#import "STPopupNavigationBar.h"
+#import "UIResponder+STPopup.h"
+#import "UIViewController+STPopup.h"
+
+FOUNDATION_EXPORT double STPopupVersionNumber;
+FOUNDATION_EXPORT const unsigned char STPopupVersionString[];
+
